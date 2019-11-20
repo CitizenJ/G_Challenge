f07a85a606e998ca9d99ddc85c5679c1483101f7
CitizenJ <jarkko_perala@yahoo.co.uk>
2019-11-21 00:15:15 +0200

    Cleaned up unnecessary crap/added comments

--- mittari_w/src/mittari_w/Serv.java
+++ mittari_w/src/mittari_w/Serv.java
@@ -11,21 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Serv
- */
 @WebServlet("/Serv")
 public class Serv extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Serv() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		URL url = new URL("http://tuftuf.gambitlabs.fi/feed.txt");
 		Scanner sc = new Scanner(url.openStream(), StandardCharsets.UTF_8.toString());
@@ -33,19 +22,12 @@ public class Serv extends HttpServlet {
 		
 			while (sc.hasNextLine())
 			{
+			//append lines and add linebreaks
 			body.append(sc.nextLine() + "<br>"); 
 			}
 		sc.close();
+		//converts to string and sends to ${content} at index.jsp 
 		request.setAttribute("content", body.toString());
 		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
